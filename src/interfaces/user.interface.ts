fe03ec9ce1153831c74b6dec92b25bf99d67f030
ikozigbo <97337028+ikozigbo@users.noreply.github.com>
2023-09-16 10:17:01 +0100

    16/09 first push

---
+++ src/interfaces/user.interface.ts
@@ -0,0 +1,14 @@
+export interface UserAttribute {
+  id: number;
+  fullname: string;
+  password: string;
+  email: string;
+  image: string;
+  status: boolean;
+  cloudId: string;
+  phoneNumber: string;
+  token: string;
+  verify: boolean;
+  updatedAt: Date;
+  createdAt: Date;
+}
