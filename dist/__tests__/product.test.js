18173017877fac7026b3909dffbbceff85d5feaf
ikozigbo <97337028+ikozigbo@users.noreply.github.com>
2023-10-01 21:15:40 +0100

    update: 01/10

--- dist/__tests__/product.test.js
+++ dist/__tests__/product.test.js
@@ -9,9 +9,12 @@ const app = (0, server_utility_1.default)();
 describe("test", () => {
     describe("get test route", () => {
         describe("given user does not exixt", () => {
-            it('should return 404 status code and message "User Not Found"', async () => {
+            it('should return 400 status code and message "User Not Found"', async () => {
                 const userId = 3;
-                await (0, supertest_1.default)(app).get(`/api/test-endpoint/${userId}`).expect(400);
+                const response = await (0, supertest_1.default)(app)
+                    .get(`/api/test-endpoint/${userId}`)
+                    .set("Authorization", `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOjEsImlhdCI6MTYjh5NjE1OTE5NSwiZXhwIjoxNjk2MjQ1NTk1fQ.uGm-e3_v-wAHhTrlsqVpkCM1DJtCfx9WP8vp3BR2zE`);
+                expect(response.status).toBe(400);
             });
         });
     });
--- src/__tests__/product.test.ts
+++ src/__tests__/product.test.ts
@@ -6,9 +6,15 @@ const app = createServer();
 describe("test", () => {
   describe("get test route", () => {
     describe("given user does not exixt", () => {
-      it('should return 404 status code and message "User Not Found"', async () => {
+      it('should return 400 status code and message "User Not Found"', async () => {
         const userId = 3;
-        await supertest(app).get(`/api/test-endpoint/${userId}`).expect(400);
+        const response = await supertest(app)
+          .get(`/api/test-endpoint/${userId}`)
+          .set(
+            "Authorization",
+            `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOjEsImlhdCI6MTYjh5NjE1OTE5NSwiZXhwIjoxNjk2MjQ1NTk1fQ.uGm-e3_v-wAHhTrlsqVpkCM1DJtCfx9WP8vp3BR2zE`
+          );
+        expect(response.status).toBe(400);
       });
     });
   });
