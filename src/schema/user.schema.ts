f27771e13667a8e931b3b4eff263875c70914709
ikozigbo <97337028+ikozigbo@users.noreply.github.com>
2023-10-06 21:52:53 +0100

    update: 06/10

---
+++ src/schema/user.schema.ts
@@ -0,0 +1,38 @@
+import { ZodSchema, number, object, string } from "zod";
+
+type userAttribute = {
+  fullname: string;
+  password: string;
+  email: string;
+  phoneNumber: string;
+};
+
+export const userSchema: ZodSchema<userAttribute> = object({
+  fullname: string({
+    required_error: "hotelName is required.",
+  })
+    .nonempty()
+    .min(2),
+  password: string({
+    required_error: "Password is required.",
+  })
+    .nonempty()
+    .min(6, "Password must be atleast six (6) characters long"),
+  email: string({
+    required_error: "email is required.",
+  })
+    .nonempty()
+    .min(2)
+    .email("Invalid email format"),
+  phoneNumber: string({
+    required_error: "phone-number is required.",
+  })
+    .regex(/^\d+$/, "Phone number must only contain numeric characters")
+    .min(10, "Phone number must be a valid number please"),
+  confirmPassword: string({
+    required_error: "required.",
+  }),
+}).refine((data) => data.password === data.confirmPassword, {
+  message: "Password does not match",
+  path: ["confirmPassword"],
+});
