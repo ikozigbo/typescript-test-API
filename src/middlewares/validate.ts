11a726ef703aced0e9ea555dcf3fc538f7c58147
ikozigbo <97337028+ikozigbo@users.noreply.github.com>
2023-10-08 23:36:21 +0100

    feat: validatot 08/10

--- src/middlewares/validate.ts
+++ src/middlewares/validate.ts
@@ -1,6 +1,44 @@
-import { z } from "zod";
+import { z, ZodError, ZodType } from "zod";
+import { RequestHandler } from "express";
 
-const mySchema = z.string();
+const schemaObj = z.object({
+  body: z.object({}),
+  query: z.object({}),
+  params: z.object({}),
+});
 
-mySchema.parse("tuna"); // => "tuna"
-mySchema.parse(12); // => throws ZodError
+type userAttribute = {
+  fullname: string;
+  password: string;
+  email: string;
+  phoneNumber: string;
+};
+
+export const validateUser =
+  (Userschema: ZodType<userAttribute>): RequestHandler =>
+  async (req, res, next) => {
+    try {
+      schemaObj.parse({
+        body: req.body,
+        query: req.query,
+        params: req.params,
+      });
+      await Userschema.parseAsync(req.body);
+      next();
+    } catch (error: any) {
+      if (error instanceof ZodError) {
+        const errorMessages = error.errors.map((err) => err.message);
+        return res.status(400).json({
+          message: errorMessages[0],
+        });
+      }
+      return res.status(500).json({
+        message: error.message,
+      });
+    }
+  };
+
+// const mySchema = z.string();
+
+// mySchema.parse("tuna"); // => "tuna"
+// mySchema.parse(12); // => throws ZodError
